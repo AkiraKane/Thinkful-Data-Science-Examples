1ed4e4f0108291225942d6688489b627067bc665
Kevin Keller <flygeneticist@gmail.com>
2015-03-19 15:41:48 +0000

    added qq plot to viz data script

--- Unit2/Lesson2/1_visualizing_data.py
+++ Unit2/Lesson2/1_visualizing_data.py
@@ -1,16 +1,17 @@
-import collections
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 # read csv file into pandas
 data = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
 data.dropna(inplace=True)
 
-# count the number of instances of open credit lines
-open_credit_freq = collections.Counter(data['Open.CREDIT.Lines'])
-
-# plot the number of open credit lines as a hist, boxplot
+# plot the Amount Requested data as a boxplot, hist
 plt.figure()
-plt.bar(open_credit_freq.keys(), open_credit_freq.values(), width=1)
-data.boxplot(column='Open.CREDIT.Lines')
+data.boxplot(column='Amount.Requested')
+data.hist(column='Amount.Requested')
+plt.show()
+# plot the same data as a qq plot
+plt.figure()
+stats.probplot(data['Amount.Requested'], dist="norm", plot=plt)
 plt.show()
