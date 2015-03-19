745b79c74bb7b61039fb15b4ef9660f91fbfe8f8
Kevin Keller <flygeneticist@gmail.com>
2015-03-19 16:13:58 +0000

    added freq redux selection on chi^2 data

--- Unit2/Lesson2/2_testing_data.py
+++ Unit2/Lesson2/2_testing_data.py
@@ -8,14 +8,20 @@ data = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData
 data.dropna(inplace=True)
 
 # count the number of instances of open credit lines
-open_credit_freq = collections.Counter(data['Open.CREDIT.Lines'])
+open_credit_freq = collections.Counter(data['Open.CREDIT.Lines']
 
 # plot the number of open credit lines as a hist
 plt.figure()
 plt.bar(open_credit_freq.keys(), open_credit_freq.values(), width=1)
 plt.show()
 
-# run a Chi-Squared Test on the data
-chi, p = stats.chisquare(open_credit_freq.values())
+# Run a Chi-Squared Test on the data
+# Step 1: only include categories with freqs greater than or equal to 5
+chi_redux = {}
+for k in open_credit_freq:
+    if open_credit_freq[k] >= 5:
+        chi_redux[k] = open_credit_freq[k]
+# Step 2: Calculate chi and p-value
+chi, p = stats.chisquare(chi_redux.values())
 print chi
 print p
