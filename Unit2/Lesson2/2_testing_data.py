9b29b51c84632230b0671a852320640bef055372
Kevin Keller <flygeneticist@gmail.com>
2015-03-19 15:44:32 +0000

    added chi^2 test to testing data script

--- Unit2/Lesson2/2_testing_data.py
+++ Unit2/Lesson2/2_testing_data.py
@@ -1,6 +1,7 @@
 import collections
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
 
 # read csv file into pandas
 data = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
@@ -13,3 +14,8 @@ open_credit_freq = collections.Counter(data['Open.CREDIT.Lines'])
 plt.figure()
 plt.bar(open_credit_freq.keys(), open_credit_freq.values(), width=1)
 plt.show()
+
+# run a Chi-Squared Test on the data
+chi, p = stats.chisquare(open_credit_freq.values())
+print chi
+print p
