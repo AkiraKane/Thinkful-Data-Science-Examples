e030d8834c875c6d785dc07edcd9e74426fae4ae
Kevin Keller <flygeneticist@gmail.com>
2015-03-19 12:50:46 +0000

    stubs for Unit 2 - Lesson 2 added

---
+++ Unit2/Lesson2/0_cleaning_data.py
@@ -0,0 +1,5 @@
+# for this lesson I will be diverging from the Thinkful material and using
+# the Kaggle Titanic dataset instead (train.csv). This is a far dirtier and,
+# in many ways, more realistic exmaple of the datasets in you might encounter.
+# I highly encourage you all to dl and work along with these examples as well as
+# working the lesson exercises.
