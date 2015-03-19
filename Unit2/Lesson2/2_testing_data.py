d2c33e1e81d35971392eb113ee23811cd20575c1
Kevin Keller <flygeneticist@gmail.com>
2015-03-19 15:37:24 +0000

    added boxplot to viz script

--- Unit2/Lesson2/2_testing_data.py
+++ Unit2/Lesson2/2_testing_data.py
@@ -6,10 +6,8 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
 data.dropna(inplace=True)
 
-# count the number of instances of open credit lines
-open_credit_freq = collections.Counter(data['Open.CREDIT.Lines'])
-
 # plot the number of open credit lines as a hist
 plt.figure()
-plt.bar(open_credit_freq.keys(), open_credit_freq.values(), width=1)
+data.boxplot(column='Amount.Requested')
+data.hist(column='Amount.Requested')
 plt.show()
